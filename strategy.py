2cf643e58b92ab7932dcc28ba9dd787a74f0a897
Cassard Mathieu <cassard.mathieu@gmail.com>
2014-05-18 16:45:48 +0200

    improve strategy class
    Signed-off-by: Cassard Mathieu <cassard.mathieu@gmail.com>

--- strategy.py
+++ strategy.py
@@ -5,9 +5,11 @@ class Strategy:
 	"""The Strategie class !"""
 
 	robot=None
+	table=None
 	finished=False
 	def __init__(self,robot):
 		self.robot=robot
+		self.table=robot.table
 
 # def recalage
 
